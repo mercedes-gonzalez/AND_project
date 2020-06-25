2054b894c3c968286300fea0016df71e8de71a83
mgonzalez91 <m.gonzalez@gatech.edu>
2020-06-25 18:55:38 -0400

    contour 'works', sharpening improved

--- enhancementMG.py
+++ enhancementMG.py
@@ -6,7 +6,6 @@
     Mercedes Gonzalez
 """
 
-import dippykit as dip
 from os.path import join, isfile
 from os import listdir
 import numpy as np
@@ -21,8 +20,7 @@ file_type = ".tiff"
 file_list = [f for f in listdir(root_path) if isfile(join(root_path, f)) & f.endswith(file_type)]
 
 filename = file_list[0]
-img = dip.im_read(join(root_path,filename)).astype('float32')
-# img = dip.imread('C:/Users/mgonzalez91/PycharmProjects/Images/cameraman.tif').astype('float32')
+img = cv2.imread(join(root_path,filename),cv2.IMREAD_GRAYSCALE)
 imgsize = img.shape
 M = imgsize[0]
 N = imgsize[1]
@@ -30,45 +28,47 @@ N = imgsize[1]
 
 # --- Various Filtering  ---------------------------------------------------------------------------
 diam = 3 # Diameter of each pixel neighborhood that is used during filtering. If it is non-positive, it is computed from sigmaSpace .
-sigma_color = 100   # Filter sigma in the color space. A larger value of the parameter means that farther colors within the pixel 
+sigma_color = 1   # Filter sigma in the color space. A larger value of the parameter means that farther colors within the pixel 
                     # neighborhood (see sigmaSpace ) will be mixed together, resulting in larger areas of semi-equal color
 sigma_space = 100   # Filter sigma in the coordinate space. A larger value of the parameter means that farther pixels will influence 
                     # each other as long as their colors are close enough (see sigmaColor ). When d>0 , it specifies the neighborhood
                     #  size regardless of sigmaSpace . Otherwise, d is proportional to sigmaSpace 
-threshold_perc = .6 # pixel intensity threshold as percentage (quantiles)
-
 
 gaussian_blur = cv2.GaussianBlur(img,(9,9),0)
 median_blur = cv2.medianBlur(img,3)
 bilateral_blur = cv2.bilateralFilter(img, diam, sigma_color, sigma_space)
 
 # --- Unsharp Algorithm  ---------------------------------------------------------------------------
-alpha = 1.9
-beta = -.9
-gamma =0
+alpha = 2
+beta = -.5
+gamma =1
 
 gaussian_unsharp = cv2.addWeighted(gaussian_blur,alpha,img,beta,gamma)
 median_unsharp = cv2.addWeighted(median_blur,alpha,img,beta,gamma)
 bilateral_unsharp = cv2.addWeighted(bilateral_blur,alpha,img,beta,gamma)
 
 # --- Masking / Thresholding ---------------------------------------------------------------------------
-_, gaussian_mask = cv2.threshold(gaussian_blur,np.quantile(img,threshold_perc),1,cv2.THRESH_TOZERO)
-_, median_mask = cv2.threshold(median_blur,np.quantile(img,threshold_perc),1,cv2.THRESH_TOZERO)
-_, bilateral_mask = cv2.threshold(bilateral_blur,np.quantile(img,threshold_perc),1,cv2.THRESH_TOZERO)
+threshold_perc = .9 # pixel intensity threshold as percentage (quantiles)
+max_percent = .99 # set max value as this % of the max intensity 
+blocksize = 151 # bigger blocksize masks bigger blobs
+C = 13 # bigger C 
+
+gaussian_mask = cv2.adaptiveThreshold(gaussian_unsharp,max_percent*np.max(gaussian_blur),cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV,blocksize,C)
+median_mask = cv2.adaptiveThreshold(median_blur,max_percent*np.max(gaussian_blur),cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV,blocksize,C)
+bilateral_mask = cv2.adaptiveThreshold(bilateral_blur,max_percent*np.max(gaussian_blur),cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV,blocksize,C)
 
 # --- Laplacian  ---------------------------------------------------------------------------
 ddepth = cv2.CV_32F
-ker = 1
+ker = 111
 gaussian_laplace = cv2.Laplacian(gaussian_blur,ddepth, ker)
 median_laplace = cv2.Laplacian(median_blur,ddepth, ker)
 bilateral_laplace = cv2.Laplacian(bilateral_blur,ddepth, ker)
 
 # --- Contouring  ---------------------------------------------------------------------------
-gaussian_contour, _ = cv2.findContours(gaussian_mask.astype('uint8'),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-median_contour, _ = cv2.findContours(median_mask.astype('uint8'),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-bilateral_contour, _ = cv2.findContours(bilateral_mask.astype('uint8'),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+gaussian_Contour, _ = cv2.findContours(gaussian_mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+median_contour, _ = cv2.findContours(median_mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+bilateral_contour, _ = cv2.findContours(bilateral_mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
 
-# ___ PLOTTING ____________________________________________________________________________
 plotFilter = 2 # 0=gauss, 1=median, 2=bilateral
 if plotFilter == 0:
     filtername = "Gaussian"
@@ -76,7 +76,7 @@ if plotFilter == 0:
     sharpened = gaussian_unsharp
     masked = gaussian_mask
     laplacian = gaussian_laplace
-    contourlist = gaussian_contour
+    contourlist = gaussian_Contour
 elif plotFilter == 1:
     filtername = "Median"
     filtered = median_blur
@@ -97,10 +97,10 @@ contour = np.copy(img) # will draw contours over original img
 print('Num contours = ',len(contourlist)) # debug
 for cnt in contourlist:
     # measure minimum enclosing circle for each contour detected
-    # (x,y),radius = cv2.minEnclosingCircle(cnt)
-    if cv2.contourArea(cnt) > 0:
-    # if cv2.contourArea(cnt) < .01*row*col and cv2.contourArea(cnt) > .0005*row*col and cv2.contourArea(cnt) > (radius*radius*3.1415*.5):
-        cv2.drawContours(contour, cnt, -1, (1,1,1), 3)
+    (x,y),radius = cv2.minEnclosingCircle(cnt)
+    if True: # debuglol
+    # if cv2.contourArea(cnt) < .1*M*N and cv2.contourArea(cnt) > .05*M*N and cv2.contourArea(cnt) > (radius*radius*3.1415*.5):
+        cv2.drawContours(contour, cnt, -1, (1,1,1), 5)
 
         # M = cv2.moments(cnt)
         # cx = int(M['m10']/M['m00'])
@@ -109,13 +109,14 @@ for cnt in contourlist:
         # centroids.append([cx, cy])
         # cv2.drawMarker(contour,(cx,cy),(color_num, color_num, color_num), cv2.MARKER_CROSS,10,1)
 
+# ___ PLOTTING ____________________________________________________________________________
 img_array = [ img, filtered, sharpened,contour, masked, laplacian]  
 title_array = [ 'Original', 'Filtered', 'Sharpened', 'Contour','Filtered Masked', 'Laplacian']
 
 fig, axs = plt.subplots(2,3)
 fig.suptitle(filtername, fontsize=20)
 axs = axs.flatten()
-fig.subplots_adjust(wspace=0,hspace=.5)
+fig.subplots_adjust(wspace=0,hspace=.1)
 for i,ax in enumerate(axs):
     title_str = title_array[i]
     ax.set_xticks([])
