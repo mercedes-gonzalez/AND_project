ca2918b9ee283c226e0ed110fe23042072255dd8
Gonzalez, Mercedes Maria <m.gonzalez@gatech.edu>
2020-07-22 18:21:48 -0400

    Update preprocessFiles.py

--- preprocessFiles.py
+++ preprocessFiles.py
@@ -6,7 +6,7 @@
     Mercedes Gonzalez
 
 """
-
+import os
 from os.path import join, isfile
 from os import listdir
 import dippykit as dip
@@ -16,31 +16,41 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import cv2
 from PIL import Image
+from xml.etree import ElementTree as et
+
+def changeXML(xmlPath,name,root_path):
+    if xmlPath is None:
+        return
+    if os.path.isfile(xmlPath) is False:
+        return
+    tree = et.parse(xmlPath)
+    tree.find('.//filename').text = name + '.png'
+    tree.find('.//path').text = root_path + name + '.png'
+    tree.write(xmlPath)
+
+
 # ____ INITIALIZATION  ____________________________________________________
 # Set path where all the images are, get list of all tiff files in that dir
-root_path = "C:/Users/mgonzalez91/Dropbox (GaTech)/Coursework/SU20 - Digital Image Processing/AND_Project/slice_images_raw/training_data/neuron/"
-save_path = "C:/Users/mgonzalez91/Dropbox (GaTech)/Coursework/SU20 - Digital Image Processing/AND_Project/slice_images_raw/training_data/neuron/"
-file_type = ".tiff"
+root_path = "C:/Users/mgonzalez91/Dropbox (GaTech)/Coursework/SU20 - Digital Image Processing/AND_Project/testing_images/untouched/"
+save_path = "C:/Users/mgonzalez91/Dropbox (GaTech)/Coursework/SU20 - Digital Image Processing/AND_Project/testing_images/histEq/"
+file_type = ".png"
 file_list = [f for f in listdir(root_path) if isfile(join(root_path, f)) & f.endswith(file_type)]
-dim = (1280,1280)
+file_list_xml = [f for f in listdir(save_path) if isfile(join(save_path, f)) & f.endswith('.xml')]
+dim = (416,416)
 
 for count,filename in enumerate(file_list):
     I = cv2.imread(join(root_path,filename),cv2.IMREAD_GRAYSCALE)
 
-    resizedI = cv2.resize(I,dim,interpolation=cv2.INTER_AREA)
-    E = np.asarray(resizedI)
+    # hist eq
+    E = np.asarray(I)
     flat = E.flatten()
-    # Find Cumulative distributive function (cdf)
     hist, bins = np.histogram(flat,256,[0,256])
     cdf = hist.cumsum()
     cdf_normalized = cdf * hist.max()/ cdf.max()
-
     cdf_num = (cdf - cdf.min()) * 255
     cdf_den = cdf.max() - cdf.min()
-    # re-normalize the cdf
     cdf_heq = cdf_num/cdf_den
     cdf_heq = cdf_heq.astype('uint8')
-
     histEq = cdf_heq[flat]
     hist2, bins2 = np.histogram(histEq,256,[0,256])
     cdf_norm_heq = cdf_heq * hist2.max()/cdf_heq.max()
@@ -48,6 +58,22 @@ for count,filename in enumerate(file_list):
 
     equalizedImage = Image.fromarray(histEqImg)
     save_filename = join(save_path,filename)
-    # equalizedImage.save(save_filename)
-    plt.imshow(equalizedImage)
-    plt.show()
+    equalizedImage.save(save_filename)
+    # break
+    # plt.subplot(1,2,1)
+    # plt.imshow(filt,cmap='gray')
+    # plt.subplot(1,2,2)
+    # plt.imshow(histEqImg,cmap='gray')
+    # plt.show()
+
+    # break
+
+# for count,filename in enumerate(file_list_xml):
+#     # new xml
+#     base,ext = os.path.splitext(filename)
+#     fullfile = join(save_path,filename)
+#     changeXML(fullfile,base,save_path)
+        
+#     print(base)
+
+    # break
