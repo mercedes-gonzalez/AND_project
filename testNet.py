dc17a269ebf792d3c38f4097cf2b80ae3afd4268
Gonzalez, Mercedes Maria <m.gonzalez@gatech.edu>
2020-07-22 14:37:42 -0400

    add text print

--- testNet.py
+++ testNet.py
@@ -12,21 +12,26 @@ from PIL import Image
 import matplotlib.pyplot as plt
 from os import listdir
 from os.path import isfile, join
+import os 
 
-def detect_img(yolo,imgPath):
+def detect_img(yolo,imgPath,logName):
     image = Image.open(imgPath)
-    r_image = yolo.detect_image(image=image)
+    r_image = yolo.detect_image(image=image,logPath=logName)
     # r_image.show()
     return r_image
 
-root_path = 'C:/Users/mgonzalez91/Dropbox (GaTech)/Coursework/SU20 - Digital Image Processing/AND_Project/testing_images/histEq_cropped_aug/'
-save_path = 'C:/Users/mgonzalez91/Dropbox (GaTech)/Coursework/SU20 - Digital Image Processing/AND_Project/testing_images/histEq_cropped_aug/annotated'
+root_path = 'C:/Users/mgonzalez91/Dropbox (GaTech)/Coursework/SU20 - Digital Image Processing/AND_Project/testing_images/histEq_input/'
+save_path = 'C:/Users/mgonzalez91/Dropbox (GaTech)/Coursework/SU20 - Digital Image Processing/AND_Project/testing_images/histEq_input/histEq_trained_net_results/'
+# root_path = 'C:/Users/mgonzalez91/Dropbox (GaTech)/Coursework/SU20 - Digital Image Processing/AND_Project/testing_images/untouched_input/'
+# save_path = 'C:/Users/mgonzalez91/Dropbox (GaTech)/Coursework/SU20 - Digital Image Processing/AND_Project/testing_images/untouched_input/histEq_trained_net_results/'
 
 file_list = [f for f in listdir(root_path) if isfile(join(root_path, f)) & f.endswith(".png")]
 my_yolo = YOLO() # start yolo session
 
 for f in file_list:
-    annotated = detect_img(my_yolo,imgPath=join(root_path,f))
+    base = os.path.basename(f)
+    logID = save_path + os.path.splitext(base)[0] + '.txt'
+    annotated = detect_img(my_yolo,imgPath=join(root_path,f),logName=logID)
     annotated.save(join(save_path,f),"PNG")
 
 my_yolo.close_session() # end yolo session
