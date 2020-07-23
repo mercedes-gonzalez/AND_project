b0d684e53938bf99ec080a3b9629e84c01e82a9a
Gonzalez, Mercedes Maria <m.gonzalez@gatech.edu>
2020-07-22 21:33:55 -0400

    Undo my last commit, revert to M.Y. version

--- AND_yolo3/yolo.py
+++ AND_yolo3/yolo.py
@@ -2,42 +2,45 @@
 """
 Class definition of YOLO_v3 style detection model on image and video
 """
-
 import colorsys
 import os
 from timeit import default_timer as timer
-
 import numpy as np
 from keras import backend as K
 from keras.models import load_model
 from keras.layers import Input
 from PIL import Image, ImageFont, ImageDraw
-
 from yolo3.model import yolo_eval, yolo_body, tiny_yolo_body
 from yolo3.utils import letterbox_image
 import os
 from keras.utils import multi_gpu_model
 from skimage import color
+
 class YOLO(object):
     _defaults = {
         # "model_path": 'C:/Users/mgonzalez91/Dropbox (GaTech)/Coursework/SU20 - Digital Image Processing/AND_Project/yolo_weights_cropped_aug/trained_weights_final.h5',
-        # "model_path": 'C:/Users/mgonzalez91/Dropbox (GaTech)/Coursework/SU20 - Digital Image Processing/AND_Project/yolo_weights_histEq_augmented/trained_weights_final.h5',
-        "model_path": 'C:/Users/mgonzalez91/Dropbox (GaTech)/Coursework/SU20 - Digital Image Processing/AND_Project/yolo_weights_histEq_only_moreepochs/trained_weights_final.h5',
+        "model_path": 'C:/Users/mgonzalez91/Dropbox (GaTech)/Coursework/SU20 - Digital Image Processing/AND_Project/yolo_weights_histEq_augmented/trained_weights_final.h5',
         "anchors_path": 'C:/Users/mgonzalez91/AND_repo/AND_Project-1/AND_yolo3/model_data/yolo_anchors.txt',
         "classes_path": 'C:/Users/mgonzalez91/AND_repo/AND_Project-1/AND_yolo3/model_data/AND_classes.txt',
+        # if on mighten comp
+        "model_path": 'logs/004/trained_weights_final.h5', #logs/004 for histEqNet; logs/005 for untouchedNet
+        "anchors_path": 'logs/005/yolo_anchors.txt',
+        "classes_path": 'model_data/AND_classes.txt',
+        # if on mercedes comp
+        # "model_path": 'C:/Users/mgonzalez91/Dropbox (GaTech)/Coursework/SU20 - Digital Image Processing/AND_Project/yolo_weights_histEq_augmented/trained_weights_final.h5',
+        # "anchors_path": 'C:/Users/mgonzalez91/AND_repo/AND_Project-1/AND_yolo3/model_data/yolo_anchors.txt',
+        # "classes_path": 'C:/Users/mgonzalez91/AND_repo/AND_Project-1/AND_yolo3/model_data/AND_classes.txt',
         "score" : 0.3, # was .3
         "iou" : 0.45, # was .45
         "model_image_size" : (416, 416),
         "gpu_num" : 1,
     }
-
     @classmethod
     def get_defaults(cls, n):
         if n in cls._defaults:
             return cls._defaults[n]
         else:
             return "Unrecognized attribute name '" + n + "'"
-
     def __init__(self, **kwargs):
         self.__dict__.update(self._defaults) # set up default values
         self.__dict__.update(kwargs) # and update with user overrides
@@ -45,25 +48,21 @@ class YOLO(object):
         self.anchors = self._get_anchors()
         self.sess = K.get_session()
         self.boxes, self.scores, self.classes = self.generate()
-
     def _get_class(self):
         classes_path = os.path.expanduser(self.classes_path)
         with open(classes_path) as f:
             class_names = f.readlines()
         class_names = [c.strip() for c in class_names]
         return class_names
-
     def _get_anchors(self):
         anchors_path = os.path.expanduser(self.anchors_path)
         with open(anchors_path) as f:
             anchors = f.readline()
         anchors = [float(x) for x in anchors.split(',')]
         return np.array(anchors).reshape(-1, 2)
-
     def generate(self):
         model_path = os.path.expanduser(self.model_path)
         assert model_path.endswith('.h5'), 'Keras model or weights must be a .h5 file.'
-
         # Load model, or construct model and load weights.
         num_anchors = len(self.anchors)
         num_classes = len(self.class_names)
@@ -78,9 +77,7 @@ class YOLO(object):
             assert self.yolo_model.layers[-1].output_shape[-1] == \
                 num_anchors/len(self.yolo_model.output) * (num_classes + 5), \
                 'Mismatch between model and given anchor and class sizes'
-
         print('{} model, anchors, and classes loaded.'.format(model_path))
-
         # Generate colors for drawing bounding boxes.
         hsv_tuples = [(x / len(self.class_names), 1., 1.)
                       for x in range(len(self.class_names))]
@@ -91,7 +88,6 @@ class YOLO(object):
         np.random.seed(10101)  # Fixed seed for consistent colors across runs.
         np.random.shuffle(self.colors)  # Shuffle colors to decorrelate adjacent classes.
         np.random.seed(None)  # Reset seed to default.
-
         # Generate output tensor targets for filtered bounding boxes.
         self.input_image_shape = K.placeholder(shape=(2, ))
         if self.gpu_num>=2:
@@ -102,6 +98,7 @@ class YOLO(object):
         return boxes, scores, classes
 
     def detect_image(self, image,logPath):
+    def detect_image(self, image, logPath, timeLog):
         start = timer()
         logFileObj = open(logPath,mode='w+')
         if self.model_image_size != (None, None):
@@ -114,11 +111,9 @@ class YOLO(object):
             boxed_image = letterbox_image(image, new_image_size)
         image_data = np.array(boxed_image, dtype='float32')
         image = image.convert("RGB")
-
         # print(image_data.shape)
         image_data /= 255.
         image_data = np.expand_dims(image_data, 0)  # Add batch dimension.
-
         out_boxes, out_scores, out_classes = self.sess.run(
             [self.boxes, self.scores, self.classes],
             feed_dict={
@@ -128,8 +123,10 @@ class YOLO(object):
             })
 
         print('Found {} boxes for {}'.format(len(out_boxes), 'img'),file=logFileObj)
+        print('Found {} boxes for {}'.format(len(out_boxes), 'img'))#,file=logFileObj)
 
         font = ImageFont.truetype(font='C:/Users/mgonzalez91/AND_repo/AND_Project-1/AND_yolo3/font/FiraMono-Medium.otf',
+        font = ImageFont.truetype(font='font/FiraMono-Medium.otf',
                     size=np.floor(3e-2 * image.size[1] + 0.5).astype('int32'))
         thickness = (image.size[0] + image.size[1]) // 300
 
@@ -137,23 +134,21 @@ class YOLO(object):
             predicted_class = self.class_names[c]
             box = out_boxes[i]
             score = out_scores[i]
-
             label = '{} {:.2f}'.format(predicted_class, score)
             draw = ImageDraw.Draw(image)
             label_size = draw.textsize(label, font)
-
             top, left, bottom, right = box
             top = max(0, np.floor(top + 0.5).astype('int32'))
             left = max(0, np.floor(left + 0.5).astype('int32'))
             bottom = min(image.size[1], np.floor(bottom + 0.5).astype('int32'))
             right = min(image.size[0], np.floor(right + 0.5).astype('int32'))
             print(label, (left, top), (right, bottom),file=logFileObj)
+            print(label, left, top, right, bottom, file=logFileObj)
 
             if top - label_size[1] >= 0:
                 text_origin = np.array([left, top - label_size[1]])
             else:
                 text_origin = np.array([left, top + 1])
-
             # My kingdom for a good redistributable image drawing library.
             for i in range(thickness):
                 draw.rectangle(
@@ -167,12 +162,14 @@ class YOLO(object):
 
         end = timer()
         print('Time to guess (s):',end - start,file=logFileObj)
+        logFileTime = open(timeLog,mode='a')
+        print(end - start, file=logFileTime)
+        logFileTime.close()
         logFileObj.close()
         return image
 
     def close_session(self):
         self.sess.close()
-
 def detect_video(yolo, video_path, output_path=""):
     import cv2
     vid = cv2.VideoCapture(video_path)
@@ -213,4 +210,3 @@ def detect_video(yolo, video_path, output_path=""):
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     yolo.close_session()
-
